d2851d01e7636e77aa4da6dbaa9e358a1e5a22e0
alliano <allianoanonymous@gmail.com>
2024-01-04 20:06:14 +0900

    test EntityManagerFactory

--- src/test/java/com/spring/data/jpa/springdatajpa/SpringDataJpaApplicationTests.java
+++ src/test/java/com/spring/data/jpa/springdatajpa/SpringDataJpaApplicationTests.java
@@ -1,13 +1,26 @@
 package com.spring.data.jpa.springdatajpa;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SpringDataJpaApplicationTests {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+
+@SpringBootTest(classes = SpringDataJpaApplication.class)
+public class SpringDataJpaApplicationTests {
+
+	private @Autowired EntityManagerFactory entityManagerFactory;
 
 	@Test
-	void contextLoads() {
+	public void testEntityManager(){
+		Assertions.assertNotNull(entityManagerFactory);
+		EntityManager entityManager = this.entityManagerFactory.createEntityManager();
+		Assertions.assertNotNull(entityManager);
+		EntityTransaction transaction = entityManager.getTransaction();
+		Assertions.assertNotNull(transaction);
+		entityManager.close();
 	}
-
 }
