0eb701360c8a8fc814d6b07c3f87ddb9aae03bab
alliano <allianoanonymous@gmail.com>
2024-01-07 11:11:53 +0900

    add unit test for transaction propagation

--- src/test/java/com/spring/data/jpa/springdatajpa/UserServiceTest.java
+++ src/test/java/com/spring/data/jpa/springdatajpa/UserServiceTest.java
@@ -4,14 +4,21 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.IllegalTransactionStateException;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.spring.data.jpa.springdatajpa.models.PaymentRequest;
+import com.spring.data.jpa.springdatajpa.services.PaymentService;
 import com.spring.data.jpa.springdatajpa.services.UserService;
 
 @SpringBootTest(classes = SpringDataJpaApplication.class)
 public class UserServiceTest {
     
     private @Autowired UserService userService;
+
+    private @Autowired PaymentService paymentService;
 
     @Test
     public void testIsert(){
@@ -31,6 +38,28 @@ public class UserServiceTest {
     public void testTransactionPropagationNever(){
         Assertions.assertThrows(ResponseStatusException.class, () -> {
             this.userService.add();
+        });
+    }
+
+    @Test
+    public void testPropagation() throws JsonMappingException, JsonProcessingException{
+        PaymentRequest paymentRequest = PaymentRequest.builder()
+                    .amount(10.0000d)
+                    .reciver("Alli")
+                    .build();
+        Assertions.assertDoesNotThrow(() -> {
+            this.userService.userTranfer(paymentRequest);
+        });
+    }
+
+    @Test
+    public void testFailPropagation() throws JsonMappingException, JsonProcessingException {
+        Assertions.assertThrows(IllegalTransactionStateException.class, () -> {
+            PaymentRequest paymentRequest = PaymentRequest.builder()
+                            .reciver("Nero")
+                            .amount(30.000d)
+                            .build();
+            this.paymentService.tranfer(paymentRequest);
         });
     }
 }
