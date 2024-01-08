6fd00249f16a9d64f0048ad9c58b133c41b08e1e
alliano <allianoanonymous@gmail.com>
2024-01-08 23:34:33 +0900

    add example for query method namming

--- src/main/java/com/spring/data/jpa/springdatajpa/repositories/PaymentRepository.java
+++ src/main/java/com/spring/data/jpa/springdatajpa/repositories/PaymentRepository.java
@@ -1,6 +1,9 @@
 package com.spring.data.jpa.springdatajpa.repositories;
 
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +11,12 @@ import com.spring.data.jpa.springdatajpa.entities.Payment;
 
 
 @Repository
-public interface PaymentRepository extends JpaRepository<Payment, String> { }
+public interface PaymentRepository extends JpaRepository<Payment, String> { 
+
+    public Optional<Payment> findAllByReciver(String reciver);
+
+    public List<Payment> findByAmountGreaterThan(Double amount);
+
+    public List<Payment> findByReciverOrderByDateDesc(String reciver);
+
+}
--- src/main/java/com/spring/data/jpa/springdatajpa/repositories/UserRepository.java
+++ src/main/java/com/spring/data/jpa/springdatajpa/repositories/UserRepository.java
@@ -1,9 +1,23 @@
 package com.spring.data.jpa.springdatajpa.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.spring.data.jpa.springdatajpa.entities.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> { }
+public interface UserRepository extends JpaRepository<User, Long> { 
+    
+    public Optional<User> findByUsernameEquals(String username);
+
+    public Optional<User> findFirstByUsername(String username);
+
+    public List<User> findByUsernameNotLike(String username);
+
+    public Optional<User> findByUsernameContaining(String key);
+
+    public Optional<User> findByUsernameAndPassword(String username, String password);
+
+}
