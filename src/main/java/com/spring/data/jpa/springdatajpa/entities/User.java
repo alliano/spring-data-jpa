5f313bfbec0e3eaa040deafa10f32dd3da725155
alliano <allianoanonymous@gmail.com>
2024-01-13 15:41:12 +0900

    createrelation one to one relation between User and Address

--- src/main/java/com/spring/data/jpa/springdatajpa/entities/User.java
+++ src/main/java/com/spring/data/jpa/springdatajpa/entities/User.java
@@ -4,9 +4,11 @@ import java.io.Serializable;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,4 +28,7 @@ public class User implements Serializable {
 
     @Column(name = "password", nullable = false)
     private String password;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private Address address;
 }
