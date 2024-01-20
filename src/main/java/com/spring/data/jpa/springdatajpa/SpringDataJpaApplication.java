effb8f5f74b6a5e13da94f77ecb650bff98b8bc3
alliano <allianoanonymous@gmail.com>
2024-01-20 14:00:26 +0900

    removing some spave

--- src/main/java/com/spring/data/jpa/springdatajpa/SpringDataJpaApplication.java
+++ src/main/java/com/spring/data/jpa/springdatajpa/SpringDataJpaApplication.java
@@ -2,7 +2,6 @@ package com.spring.data.jpa.springdatajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class SpringDataJpaApplication {
 
